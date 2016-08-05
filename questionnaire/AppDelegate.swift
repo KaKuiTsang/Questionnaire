64ea022bbe7287ba3d09ad50860a0f48eb0be2a8
Tsang Ka Kui <kakuitsang2010@gmail.com>
2016-08-05 22:33:39 +0800

    project init

--- questionnaire/AppDelegate.swift
+++ questionnaire/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.makeKeyAndVisible()
+        let viewController = ViewController()
+        let rootController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = rootController
         return true
     }
 
